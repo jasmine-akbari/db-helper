af6b80d6e5c7209b7f4f7fd9e1a532069362ece7
Jasmine Akbari <jasmine_akbari@amesite.com>
2021-09-23 09:57:58 -0400

    created psql db and ran schema

---
+++ main.py
@@ -0,0 +1,6 @@
+import csv
+
+acceptedFile = input('Please provide the name of the locally saved article CSV file.' 
+'Please make sure the PDF File you would like to search is saved in the same location you saved this application')
+
+filename = f'../{acceptedFile}.csv'
